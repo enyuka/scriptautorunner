793ae52df38a902024ec5d0cf2277fd659c44cc5
enyuka <nakamura345@gmail.com>
2023-10-20 10:48:17 +0900

    Add クリップボードに URL をいい感じにコピーするスクリプト

---
+++ zendesk/copyURLOnlyArticleId.js
@@ -0,0 +1,20 @@
+// 現在のURLを取得
+const url = location.href;
+
+// URLからhttpsから記事番号までを取得
+const match = url.match(/(https:\/\/.+\/articles\/\d+)/);
+
+if (match) {
+  console.log(match);
+  setTimeout(() => {
+    // クリップボードにコピー
+    navigator.clipboard.writeText(match[1]).then(() => {
+      console.log("URLがクリップボードにコピーされました！😊");
+    }).catch((err) => {
+      console.log("エラーが発生しました。😢");
+      console.error(err);
+    });
+  }, 1000);
+} else {
+  console.log("記事番号が見つかりませんでした。😔");
+}
